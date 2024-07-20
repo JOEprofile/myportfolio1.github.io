ff5cb90e995b974c3973e6e9f35d460be366f495
faizan3332 <138961960+faizan3332@users.noreply.github.com>
2024-07-20 18:42:06 +0530

    mythis

---
+++ scrs.js
@@ -0,0 +1,94 @@
+<div class="swiper-container testimonial-swiper">
+                  <div class="swiper-wrapper">
+                   <!--  <div class="swiper-slide testimonial-box yellow">
+                      <h1>Pixel Perfect design</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p>
+                      <div class="text-right">
+                        <p class="name">- John Doe</p>
+                        <p class="designation">Founder, Arrow</p>
+                      </div>
+                    </div> -->
+                   <!--  <div class="swiper-slide testimonial-box purple">
+                      <h1>Minimal and Clean</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p>
+                      <div class="text-right">
+                        <p class="name">- Rocky Hych</p>
+                        <p class="designation">CTO, Upside Down</p>
+                      </div>
+                    </div> -->
+                   <!--  <div class="swiper-slide testimonial-box blue">
+                      <h1>Built with Bootstrap</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p>
+                      <div class="text-right">
+                        <p class="name">- John Doe</p>
+                        <p class="designation">Founder, Arrow</p>
+                      </div>
+                    </div> -->
+                <!--     <div class="swiper-slide testimonial-box pink">
+                      <h1>Responsive template</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p>
+                      <div class="text-right">
+                        <p class="name">- Rocky Hych</p>
+                        <p class="designation">CTO, Upside Down</p>
+                      </div>
+                    </div> -->
+                   <!--  <div class="swiper-slide testimonial-box yellow">
+                      <h1>Pixel Perfect</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </p>
+                      <div class="text-right">
+                        <p class="name">- John Doe</p>
+                        <p class="designation">Founder, Arrow</p>
+                      </div>
+                    </div> -->
+                   <div class="swiper-slide testimonial-box purple">
+                      <h1>Abdul Faizan</h1>
+                      <p align="justify"> My name is Abdul Faizan, and I am a assionate full stack web developer with 1 year of valuable experience. I reside in Amravati, Maharashtra, and I am dedicated to continuously enhancing my skills. I enjoy working with new technologies and tools, always committed to writing innovative solutions and efficient code.</p>
+                      <div class="text-right">
+                        <p class="name">-Faizan</p>
+                      </div>
+                    </div> 
+                    <div class="swiper-slide testimonial-box blue">
+                      
+                      <h1>My Enducation</h1>
+                      <p align="justify">My name is Abdul Faizan, and I am currently a Bachelor of Science in Computer Science student. I have completed my studies in Amravati, Maharashtra. I am passionate about learning and exploring new technologies, and I am dedicated to enhancing my skills in the field of computer science.</p>
+                      <div class="text-right">
+                        <p class="name" style="font-size: small;">- Bachelor of Science in Computer Science student</p>
+                   
+                      </div>
+                     
+                    </div>
+                    <div class="swiper-slide testimonial-box pink">
+                      <h1>My coding Field</h1>
+                      <p>Programming language &#8594;</p>
+                      <p style="color: #b081ee;
+      font-family: 'Montserrat', sans-serif; text-align: center;"> C and C++ , Python</p>
+          <p>Full Stack Developer &#8594;</p>
+          <p style="color: #b081ee;
+  font-family: 'Montserrat', sans-serif; text-align: center;"> Front-End and back-End
+  </p>
+                       <div class="text-right">
+                        <p class="name">- Certificate</p>
+                        <p class="designation">web Development</p>
+                      </div>
+                      <p ><a onclick="javascript:ShowHide('HiddenDiv')">read more</a></p>
+  <div class="mid" id="HiddenDiv" style="display: none; "><font face="Arial" size="+2" 
+   align="right">I am a versatile software developer with a strong foundation in multiple programming languages, including C, C++, and Python. My expertise spans both low-level and high-level programming, allowing me to tackle a wide range of technical challenges. Additionally, I am a proficient full-stack web developer, skilled in creating dynamic and responsive web applications.
+  </div>
+  <script type="text/javascript">
+  function ShowHide(divId)
+  {
+  if(document.getElementById(divId).style.display == 'none')
+  {
+  document.getElementById(divId).style.display='block';
+  }
+  else
+  {
+  document.getElementById(divId).style.display = 'none';
+  }
+  }
+  </script>
+                    </div>
+                
+                  </div>
+               
+                  </div>
